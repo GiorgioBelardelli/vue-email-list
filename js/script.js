2dff78e4f6b0e8c128f3a768639edf5e552dad63
GiorgioBelardelli <asterapocalypse@gmail.com>
2023-12-13 16:56:37 +0100

    feats: - for cycle + axios get - v-if mails.length === 10

--- js/script.js
+++ js/script.js
@@ -3,17 +3,28 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            mails: []
+            mails: [],
+            mailProva: "" 
         }
     },
     methods:{
 
     },
     mounted(){
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(function(risposta){
-            console.log(risposta.data.response);
-            //this.mails.push(risposta)
+
+        for(i=0; i<10; i++){
+            
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((risposta) => {
+            
+            this.mailProva = risposta.data.response;
+            this.mails.push(this.mailProva);
+
+            //DEBUG
+            // console.log(risposta.data.response);
+            // console.log(this.mails);
         })
+        }
+        
     }
 }).mount('#app')
